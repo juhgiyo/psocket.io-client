6991867c588a82ed7dae8f9f4c598dc3024b0859
Woong Gyu La <juhgiyo@gmail.com>
2015-10-21 17:02:41 +0900

    initial commit

---
+++ support/browserify.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var browserify = require('browserify');
+var path = require.resolve('../');
+
+/**
+ * Module exports.
+ */
+
+module.exports = build;
+
+/**
+ * Make the build.
+ *
+ * @api public
+ */
+
+
+function build(fn){
+    var opts = {};
+    opts.builtins = false;
+    opts.entries = [path];
+    opts.standalone = 'io';
+    opts.insertGlobalVars = { global: glob };
+    browserify(opts).bundle(fn);
+}
+
+/**
+ * Populates `global`.
+ *
+ * @api private
+ */
+
+function glob(){
+    return 'typeof self !== "undefined" ? self : '
+        + 'typeof window !== "undefined" ? window : {}';
+}
